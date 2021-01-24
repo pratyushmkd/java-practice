65e8bea4990925cafbb43c01bede1a9ebaafb672
Pratyush Manohar Kund <pratyush.mkd@gmail.com>
2021-01-24 15:36:18 +0000

    1st class

--- Main.java
+++ Main.java
@@ -2,11 +2,12 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.lang.*;
+
 class Main {
   public static void main(String[] args) {
 
     List<Integer> usrList = Arrays.asList(4, 4, 4, 4, 4);
-    List<Integer> perRoleList =  Arrays.asList(1, 4, 4, 4, 4);
+    List<Integer> perRoleList = Arrays.asList(1, 4, 4, 4, 4);
     List<Integer> unSyncList = new ArrayList<>(usrList);
     unSyncList.removeAll(perRoleList);
 
